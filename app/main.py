4757c6043084e000a28ef66422accf4816ad6dc8
HaiBQ <bqhai.199x@gmail.com>
2025-07-23 00:33:50 +0700

    add lifespan

--- app/main.py
+++ app/main.py
@@ -2,12 +2,22 @@ from fastapi import FastAPI
 from app.middlewares.exception import ExceptionMiddleware
 from app.core.logging import setup_logging
 from app.api.main import api_router
-
-
-app = FastAPI(title="FastAPI Project")
+from alembic.config import Config
+from alembic import command
+from contextlib import asynccontextmanager
+import asyncio
 
 # Setup logging
-setup_logging()
+log = setup_logging()
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    await asyncio.to_thread(command.upgrade, Config("alembic.ini"), "head")
+    yield
+
+
+app = FastAPI(title="FastAPI Project", lifespan=lifespan)
 
 
 # Add middleware
